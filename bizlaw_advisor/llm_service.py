fe5147272c87b568304d5c9f62bf8b29b005d9bf
Pratham Jain <13prathamjain@gmail.com>
2025-09-09 12:41:39 +0530

    updated

--- bizlaw_advisor/llm_service.py
+++ bizlaw_advisor/llm_service.py
@@ -93,7 +93,7 @@ class LLMService:
             "overlapping_regulations": "List of Identified overlapping regulations"
         }
         # Generate response
-        result = chain.run(
+        result = chain.invoke(
             business_type=context.business_type,
             city=context.city,
             state=context.state,
